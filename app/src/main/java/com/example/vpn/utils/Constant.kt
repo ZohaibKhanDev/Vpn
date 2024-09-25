12d201a71e5b71022c7a7ee87e3a99c0d9438249
ZohaibKhanDev <mzkhan9610@gmail.com>
2024-09-25 15:09:59 +0500

    Screen Shots

--- app/src/main/java/com/example/vpn/utils/Constant.kt
+++ app/src/main/java/com/example/vpn/utils/Constant.kt
@@ -3,6 +3,6 @@ package com.example.vpn.utils
 object Constant {
     const val TIMEOUT = 30000L
     const val API_KEY = "e4e7d86908b2486fa1bdc74cc1418c3d"
-    const val DOWNLOAD = 4.3
-    const val UPLOAD = 5.9
+    const val DOWNLOAD = 2.6
+    const val UPLOAD = 3.5
 }
